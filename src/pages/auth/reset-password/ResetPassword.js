9c28772cbc3e08b5512bdeee65fa7672cae7c78b
RyoHoang <ryohoang@motorist.vn>
2024-10-14 20:55:26 +0700

    Add ResetPassword - Config ChakraUI

--- src/pages/auth/reset-password/ResetPassword.js
+++ src/pages/auth/reset-password/ResetPassword.js
@@ -1,100 +1,55 @@
-import '@pages/auth/reset-password/ResetPassword.scss';
-import { useState } from 'react';
-import { FaArrowLeft } from 'react-icons/fa';
-import Input from '@components/input/Input';
-import Button from '@components/button/Button';
-import { Link, useSearchParams } from 'react-router-dom';
-import backgroundImage from '@assets/images/background.jpg';
-import { authService } from '@services/api/auth/auth.service';
+import {
+  Button,
+  ChakraProvider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue
+} from '@chakra-ui/react';
 
 const ResetPassword = () => {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertType, setAlertType] = useState('');
-  const [responseMessage, setResponseMessage] = useState('');
-  const [searchParams] = useSearchParams();
-
-  const resetPassword = async (event) => {
-    setLoading(true);
-    event.preventDefault();
-    try {
-      const body = { password, confirmPassword };
-      const response = await authService.resetPassword(searchParams.get('token'), body);
-      setLoading(false);
-      setPassword('');
-      setConfirmPassword('');
-      setShowAlert(false);
-      setAlertType('alert-success');
-      setResponseMessage(response?.data?.message);
-    } catch (error) {
-      setAlertType('alert-error');
-      setLoading(false);
-      setShowAlert(true);
-      setResponseMessage(error?.response?.data?.message);
-    }
-  };
-
   return (
-    <div className="container-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="environment">DEV</div>
-      <div className="container-wrapper-auth">
-        <div className="tabs reset-password-tabs" style={{ height: `${responseMessage ? '400px' : ''}` }}>
-          <div className="tabs-auth">
-            <ul className="tab-group">
-              <li className="tab">
-                <div className="login reset-password">Reset Password</div>
-              </li>
-            </ul>
-            <div className="tab-item">
-              <div className="auth-inner">
-                {responseMessage && (
-                  <div className={`alerts ${alertType}`} role="alert">
-                    {responseMessage}
-                  </div>
-                )}
-                <form className="reset-password-form" onSubmit={resetPassword}>
-                  <div className="form-input-container">
-                    <Input
-                      id="password"
-                      name="password"
-                      type="password"
-                      value={password}
-                      labelText="New Password"
-                      placeholder="New Password"
-                      style={{ border: `${showAlert ? '1px solid #fa9b8a' : ''}` }}
-                      handleChange={(e) => setPassword(e.target.value)}
-                    />
-                    <Input
-                      id="cpassword"
-                      name="cpassword"
-                      type="password"
-                      value={confirmPassword}
-                      labelText="Confirm Password"
-                      placeholder="Confirm Password"
-                      style={{ border: `${showAlert ? '1px solid #fa9b8a' : ''}` }}
-                      handleChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                  </div>
-                  <Button
-                    label={`${loading ? 'RESET PASSWORD IN PROGRESS...' : 'RESET PASSWORD'}`}
-                    className="auth-button button"
-                    disabled={!password || !confirmPassword}
-                  />
-
-                  <Link to={'/'}>
-                    <span className="login">
-                      <FaArrowLeft className="arrow-left" /> Back to Login
-                    </span>
-                  </Link>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ChakraProvider>
+      {' '}
+      <Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack
+          spacing={4}
+          w={'full'}
+          maxW={'md'}
+          bg={useColorModeValue('white', 'gray.700')}
+          rounded={'xl'}
+          boxShadow={'lg'}
+          p={6}
+          my={12}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+            Enter new password
+          </Heading>
+          <FormControl id="email" isRequired>
+            <FormLabel>Email address</FormLabel>
+            <Input placeholder="your-email@example.com" _placeholder={{ color: 'gray.500' }} type="email" />
+          </FormControl>
+          <FormControl id="password" isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" />
+          </FormControl>
+          <Stack spacing={6}>
+            <Button
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500'
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    </ChakraProvider>
   );
 };
 
