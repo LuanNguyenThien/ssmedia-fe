4b5cc763ef8225daf8689f3f61e477a571f04586
tamnguyen1411 <chuotvip233@gmail.com>
2024-12-14 15:27:51 +0700

    loading khi searh

--- src/pages/social/search/SearchPage.js
+++ src/pages/social/search/SearchPage.js
@@ -3,7 +3,9 @@ import { useLocation } from 'react-router-dom';
 import { postService } from '@services/api/post/post.service';
 import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import Post from '@components/posts/post/Post';
+import { Spinner } from '@chakra-ui/react'; // Dùng spinner từ Chakra UI
 import '@pages/social/search/SearchPage.scss';
+
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -33,12 +35,32 @@ const SearchPage = () => {
 
   return (
     <div className="search-page" ref={bodyRef}>
-      {/* Render search results */}
+      {loading && searchResults.length === 0 && (
+        <div className="search-page__loading">
+          <Spinner width="50px" height="50px" />
+          <p>Đang tìm kiếm...</p>
+        </div>
+      )}
+
+      {!loading && searchResults.length === 0 && (
+        <div className="search-page__no-results">
+          <p>Không tìm thấy kết quả nào</p>
+        </div>
+      )}
+
       {searchResults
         .filter((post) => post.privacy !== 'Private')
         .map((post) => (
           <Post key={post.id} post={post} showIcons={false} className="search-page__post" />
         ))}
+
+      {loading && searchResults.length > 0 && (
+        <div className="search-page__loading-more">
+          <Spinner width="10px" height="10px" />
+          <p>Đang tải thêm bài viết...</p>
+        </div>
+      )}
+
       <div ref={bottomLineRef} className="search-page__bottom-line"></div>
     </div>
   );
