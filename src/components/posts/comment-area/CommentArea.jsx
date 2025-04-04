f049d2a16e695114dce8bb8a89217df4086bee51
tamnguyen1411 <chuotvip233@gmail.com>
2025-04-04 10:02:17 +0700

    save page, stream rps

--- src/components/posts/comment-area/CommentArea.jsx
+++ src/components/posts/comment-area/CommentArea.jsx
@@ -242,8 +242,8 @@ const CommentArea = ({ post }) => {
           onClick={() => addReactionPost("upvote")}
         >
           <FaArrowUp className="w-5 h-5" />
-          <span className="font-medium">Upvote</span>
-          <span className="text-gray-500 mx-0.5">·</span>
+          <span className="font-medium hidden sm:block">Upvote</span>
+          <span className="text-gray-500 mx-0.5 hidden sm:block">·</span>
           <span className='"text-gray-500"'>{post.reactions["upvote"]}</span>
         </button>
 
