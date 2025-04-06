c5d1aefc42946e892e1a768ec8aade1e6d666bbd
tamnguyen1411 <chuotvip233@gmail.com>
2025-04-06 16:03:17 +0700

    comment input, fix undefind icon

--- src/components/chat/image-preview/ImagePreview.jsx
+++ src/components/chat/image-preview/ImagePreview.jsx
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types';
 import { FaTimes } from 'react-icons/fa';
 import '@components/chat/image-preview/ImagePreview.scss';
-
-const ImagePreview = ({ image, onRemoveImage }) => {
+const ImagePreview = ({ image, onRemoveImage, onClick }) => {
   return (
     <div className="image-preview-container" data-testid="image-preview">
       <div className="image-preview">
-        <img className="img" src={image} alt="" />
+        <img
+          className="img cursor-pointer"
+          src={image}
+          alt=""
+          onClick={onClick} 
+        />
         <FaTimes className="icon" onClick={onRemoveImage} />
       </div>
     </div>
   );
 };
+
 
 ImagePreview.propTypes = {
   image: PropTypes.string,
