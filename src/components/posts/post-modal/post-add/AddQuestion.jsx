47442d748d07e87a168d6d532dce2fc3e683ff3e
LuanNguyen <thienluan678@gmail.com>
2025-04-21 14:52:39 +0700

    fix addquestion for better layout on medium screens

--- src/components/posts/post-modal/post-add/AddQuestion.jsx
+++ src/components/posts/post-modal/post-add/AddQuestion.jsx
@@ -136,7 +136,7 @@ export default function AddQuestion() {
           </div>
         )}
         <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-50 p-4">
-          <div className="bg-white rounded-lg shadow-xl w-full sm:w-4/5 md:w-2/5 flex flex-col h-[75vh] sm:h-[90vh]">
+          <div className="bg-white rounded-lg shadow-xl w-full sm:w-4/5 md:w-1/2 flex flex-col h-[75vh] sm:h-[90vh]">
             <div className="p-4 border-b border-gray-200 flex justify-between items-center">
               <div className="relative w-fit">
                 <select
